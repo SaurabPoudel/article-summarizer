c28b79728e9c7deced94269a757ef0dfa1cc064c
SaurabPoudel <poudelsaurab20@gmail.com>
2023-10-30 21:35:35 +0545

    Added github url to the project

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <img src={logo} alt="sumz_logo" className='w-28 object-contain'/>
         <button
           type='button'
-          onClick={()=> window.open('https://github.com/SaurabPoudel')}
+          onClick={()=> window.open('https://github.com/SaurabPoudel/article-summarizer')}
           className='black_btn'
         >
           Github
